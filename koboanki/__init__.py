adbb637e968a6c4f198fe330b0a09b53edf0c3b5
Awes Mubarak <contact@awesmubarak.com>
2021-05-28 15:54:29 +0100

    Add initial file structure

--- koboanki/__init__.py
+++ koboanki/__init__.py
@@ -1,1 +1,14 @@
-__version__ = '0.1.0'
+from aqt import mw
+from aqt.utils import showInfo, qconnect
+from aqt.qt import *
+
+def akMenuAction() -> None:
+    cardCount = mw.col.cardCount()
+    showInfo("Card count: %d" % cardCount)
+
+    # open file selection menu
+
+
+action = QAction("Import KOBO wordlist", mw)
+qconnect(action.triggered, akMenuAction)
+mw.form.menuTools.addAction(action)
