f08da639137c9c99e8ac163d47e8496e58f61f84
Awes Mubarak <contact@awesmubarak.com>
2021-05-29 20:16:44 +0100

    Add dictionary lookup

--- koboanki/__init__.py
+++ koboanki/__init__.py
@@ -6,11 +6,14 @@ from aqt import forms
 from anki.importing.base import Importer
 import sqlite3
 
+import requests
+
 def akMenuAction() -> None:
     cardCount = mw.col.cardCount()
     forms.importing.Ui_ImportDialog()
 
     # open file selection menu
+    # TODO: open menu for this
     dir_loc = "/Volumes/KOBOeReader"
     file_loc = dir_loc + "/.kobo/KoboReader.sqlite"
 
@@ -19,8 +22,18 @@ def akMenuAction() -> None:
     cursor = connection.cursor()
     rows = [row[0] for row in cursor.execute("SELECT text from WordList").fetchall()]
 
+    # find definitions
+    # TODO: use an acctual module for this lmao
+    for word in rows:
+        response = requests.get(f"https://api.dictionaryapi.dev/api/v2/entries/en_US/{word}").json()
+        try:
+            definition = response[0]["meanings"][0]["definitions"][0]["definition"]
+        except:
+            pass
+
     # add the words to the right file
 
+    showInfo("Done importing!")
 
 
 action = QAction("Import KOBO wordlist", mw)
