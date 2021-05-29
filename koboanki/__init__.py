c15d94f0a361e74931f0adef86d65d1a0f941c29
Awes Mubarak <contact@awesmubarak.com>
2021-05-29 17:03:16 +0100

    Import rows from the words list
    No selection menu for the reader so won't work on anything but osx.

--- koboanki/__init__.py
+++ koboanki/__init__.py
@@ -1,12 +1,26 @@
 from aqt import mw
-from aqt.utils import showInfo, qconnect
+from aqt.utils import showInfo, qconnect, tr
 from aqt.qt import *
+import anki.importing as importing
+from aqt import forms
+from anki.importing.base import Importer
+import sqlite3
 
 def akMenuAction() -> None:
     cardCount = mw.col.cardCount()
-    showInfo("Card count: %d" % cardCount)
+    forms.importing.Ui_ImportDialog()
 
     # open file selection menu
+    dir_loc = "/Volumes/KOBOeReader"
+    file_loc = dir_loc + "/.kobo/KoboReader.sqlite"
+
+    # read in the file list
+    connection = sqlite3.connect(file_loc)
+    cursor = connection.cursor()
+    rows = [row[0] for row in cursor.execute("SELECT text from WordList").fetchall()]
+
+    # add the words to the right file
+
 
 
 action = QAction("Import KOBO wordlist", mw)
