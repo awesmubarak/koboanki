d8079afbd625555b020d4766524c01ca11df3468
Awes Mubarak <contact@awesmubarak.com>
2021-05-31 16:42:23 +0100

    Improve error detection and remove dupe confirm

---
+++ CHANGELOG.md
@@ -0,0 +1,14 @@
+# CHANGELOG.md
+
+## Unreleased
+
+Fixed:
+
+- Remove duplicate confirmation popups
+- Improved error handling
+
+## 1.0.0 (2021-05-31)
+
+Features:
+
+- Initial release!
--- koboanki/__init__.py
+++ koboanki/__init__.py
@@ -7,10 +7,12 @@ from aqt.qt import *
 import sqlite3
 import requests
 from os import path
-from PyQt5.QtWidgets import QFileDialog, QWidget
+from PyQt5.QtWidgets import QFileDialog
+from sys import exit
 
 
 def getWordDefinition(word: str) -> str:
+    """Return the definition of a word that's passed to it. Empty if no definition"""
     # TODO: add citation (https://www.lexico.com/about)
     response = requests.get(
         f"https://api.dictionaryapi.dev/api/v2/entries/en_US/{word}"
@@ -23,58 +25,89 @@ def getWordDefinition(word: str) -> str:
     return definition
 
 
-def akMenuAction() -> None:
-    cardCount = mw.col.cardCount()
-
-    # get folder name
+def getFileName() -> str:
+    """Returns a file location for the sqlite3 database. Empty if error or not found."""
     folderName = QFileDialog.getExistingDirectory(
         None, "Select KOBO drive", path.expanduser("~"), QFileDialog.ShowDirsOnly
     )
-    if not folderName:
-        return 0
 
-    fileLocation = path.join(folderName, ".kobo", "KoboReader.sqlite")
-    if not (path.exists(fileLocation) and path.isfile(fileLocation)):
-        showInfo(f"File path not found: {fileLocation}")
-        return 1
+    if folderName:
+        fileLocation = path.join(folderName, ".kobo", "KoboReader.sqlite")
+        if not (path.exists(fileLocation) and path.isfile(fileLocation)):
+            showInfo(f"File path not found: {fileLocation}")
+            fileLocation = ""
+    else:
+        fileLocation = ""
 
-    # read in the file list
+    return fileLocation
+
+
+def getKoboWordList(fileLocation: str) -> list:
+    """Opens the kobo file and returns a list of saved words."""
     connection = sqlite3.connect(fileLocation)
     cursor = connection.cursor()
     wordlist = [
         row[0] for row in cursor.execute("SELECT text from WordList").fetchall()
     ]
+    return wordlist
 
-    # find repeated words and don't use them because
-    ankiWordList = []
 
+def getNeworldList(koboWordList: list) -> list:
+    """Returns a list of only words not already added to anki."""
     ids = mw.col.find_notes("")
     ankiWordList = [mw.col.getNote(id_).items()[0][1] for id_ in ids]
-    newWords = [word for word in wordlist if word not in ankiWordList]
+    newWords = [word for word in koboWordList if word not in ankiWordList]
+    return newWords
 
-    # find definitions
-    addedWords = []
-    failedWords = []
-    for word in newWords:
+
+def getDefinitions(wordList: list) -> tuple:
+    """Finds the definition for each word. If not definition is found adds to a list."""
+    wordDict = {}
+    failedWods = []
+    for word in wordList:
         definition = getWordDefinition(word)
         if definition:
-            note = mw.col.newNote()
-            note["Front"] = word
-            note["Back"] = definition
-            mw.col.addNote(note)
-
-            addedWords.append(word)
+            wordDict[word] = definition
         else:
-            failedWords.append(word)
+            failedWods.append(word)
+    return (wordDict, failedWods)
 
-    # add the words to the right file
-    # TODO: this just adds it to a default one i think? idk how this works
+
+def addToCol(wordDict: dict) -> None:
+    """Adds valid words to the collection"""
+    for word, definition in wordDict.items():
+        note = mw.col.newNote()
+        note["Front"] = word
+        note["Back"] = definition
+        mw.col.addNote(note)
+
     mw.col.save()
+    return
+
+
+def akMenuAction() -> None:
+    """Main function, binds to menu item"""
+    # get folder name
+    fileLocation = getFileName()
+
+    if not fileLocation:
+        return
+
+    # read in the file list
+    wordlist = getKoboWordList(fileLocation)
+
+    if not wordlist:
+        showInfo("No saved words found")
+        return
+
+    # find newwords, get definitions, add to collection
+    newWords = getNeworldList(wordlist)
+    wordDict, failedWords = getDefinitions(newWords)
+    addToCol(wordDict)
 
     # done
-    showInfo("Done importing!")
     showInfo(
-        f"Added words: {[w for w in addedWords]}\n\nFailed words: {[w for w in failedWords]}"
+        f"Added words: {[w for w in wordDict.keys()]}\n\nFailed words: {[w for w in failedWords]}"
     )
 
 
