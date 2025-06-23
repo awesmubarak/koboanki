361cd388e56f5b6fcb4a887881a37dba8806c580
Awes Mubarak <contact@awesmubarak.com>
2025-06-23 11:14:29 +0100

    chore: reset repository for v3 rewrite (empty state)

--- dev/release.sh
+++
@@ -1,3 +0,0 @@
-set -euf -o pipefail
-
-cd ../koboanki && zip ../dev/koboanki.ankiaddon __init__.py config.json config.md manifest.json
--- dev/symlink.sh
+++
@@ -1,3 +0,0 @@
-set -euf -o pipefail
-
-ln -s ~/Projects/koboanki/koboanki ~/Library/Application\ Support/Anki2/addons21/koboanki
